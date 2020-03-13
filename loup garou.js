f4041318c9bc7a96306a6bb5382a131ca5232324
Jungkow <62137067+Jungkow@users.noreply.github.com>
2020-03-13 11:50:27 +0100

    Add files via upload

--- loup garou.js
+++ loup garou.js
@@ -1,1 +1,16 @@
+function suivant(){
+	if(etat==0){
+		alert("Le village s'endort")
+		etat = 1;
+		suivant();
+	}
+	if(etat==1){
+		console.log("Robin le gros chien")
+		etat = 1;
+		suivant();
+	}
+}
 
+var etat = 0 ;
+
+suivant();
